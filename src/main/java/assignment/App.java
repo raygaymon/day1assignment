c24e9a720f84b4708ef70a1bcfccbfc5f84db1e8
raygaymon <raysonlim.jr@gmail.com>
2023-04-18 14:45:32 +0800

    end of assignment

--- src/main/java/assignment/App.java
+++ src/main/java/assignment/App.java
@@ -4,57 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         List<String> cart = new ArrayList<>();
         String op = "";
-        String fruit= "";
+        String fruits = "";
         System.out.println("Welcome to your shopping cart");
 
-        Scanner scan = new Scanner (System.in);
+        Scanner scan = new Scanner(System.in);
 
         while (!"checkout".equals(op)) {
             op = scan.next();
-            fruit = scan.nextLine().trim();
+            fruits = scan.nextLine().trim();
 
-         if ("list".equals(op)) {
-            if (cart.isEmpty()) {
-                System.out.println("Your cart is empty");
-            }
-            else {
-                for (int i = 0; i < cart.size(); i++) {
-                    System.out.printf("%d. %s\n", i+1, cart.get(i));
-                }
-
-                }
-            }
-         else if ("add".equals(op)) {
-            if (!cart.contains(fruit)) {
-            cart.add(fruit);
-            System.out.println(fruit +" added to cart");
-        } else {
-            System.out.println ("You have " + fruit + " in your cart");
-             }
-        } else if ("delete".equals(op)) {
-            int a = Integer.valueOf(fruit);
-            for (int i = 0; i < cart.size(); i++) {
-                if (a==i) {
-                    cart.remove(cart.get(a-1));
-                    System.out.println(fruit + " has been removed");
-                    break;
+            if ("list".equals(op)) {
+                if (cart.isEmpty()) {
+                    System.out.println("Your cart is empty");
                 } else {
-                System.out.println("Incorrect item index");
+                    for (int i = 0; i < cart.size(); i++) {
+                        System.out.printf("%d. %s\n", i + 1, cart.get(i));
+                    }
+                }
             }
-             if ("checkout".equals(op)) {
-                System.out.println ("Proceeding to payment");
-                break;
+
+            else if ("add".equals(op)) {
+                if (!cart.contains(fruits)) {
+                    String[] manyFruits = fruits.split(", |,|//s+");
+                    for (int i = 0; i < manyFruits.length; i++) {
+                        cart.add(manyFruits[i]);
+                    }
+                    System.out.println(fruits + " added to cart");
+                } else {
+                    System.out.println("You already have " + fruits + " in your cart");
+                }
             }
-        }
+
+            else if ("delete".equals(op)) {
+                int a = Integer.valueOf(fruits);
+                if (a <= cart.size()) {
+                    cart.remove(cart.get(a - 1));
+                    System.out.println(fruits + " has been removed");
+                } else {
+                    System.out.println("Incorrect item index");
+                    }
+            }
+               if ("checkout".equals(op)) {
+                System.out.println("Proceeding to payment");
+                break; 
+                } 
+            } 
     }
 }
